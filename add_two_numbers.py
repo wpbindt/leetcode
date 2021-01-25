bfe35b2817ccc680771991dc248db320337b438d
wpbindt <wesselbindt@gmail.com>
2021-01-25 22:22:39 +0100

    Fix and oopify add two numbers

--- add_two_numbers.py
+++ add_two_numbers.py
@@ -1,31 +1,30 @@
 from __future__ import annotations
 from typing import Optional
 
+
 class ListNode:
     def __init__(self, val: int, next: Optional[ListNode] = None) -> None:
         self.val = val
         self.next = next
 
+    def to_int(self) -> int:
+        if self.next is None:
+            return self.val
+        return self.val + 10 * self.next.to_int()
 
-def list_to_int(l: Optional[ListNode]) -> int:
-    if l is None:
-        return 0
+    @classmethod
+    def from_int(cls, n: int) -> ListNode:
+        if 0 <= n <= 9:
+            return cls(n)
 
-    return l.val + 10 * list_to_int(l)
-
-
-def int_to_list(n: int) -> LinkedList:
-    if 0 <= n <= 9:
-        return LinkedList(n)
-
-    return LinkedList(n % 10, int_to_list(n // 10))
+        return cls(n % 10, cls.from_int(n // 10))
 
 
 def add_two_numbers(l1: ListNode, l2: ListNode) -> ListNode:
     """
     O(n + m) with n and m the respective lengths of the linked lists
     """
-    n1 = list_to_int(l1)
-    n2 = list_to_int(l2)
-    return int_to_list(n1 + n2)
+    n1 = l1.to_int()
+    n2 = l2.to_int()
+    return ListNode.from_int(n1 + n2)
 
