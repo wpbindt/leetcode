78b72182b67075c4fbf0092bffdd0d2919944a88
wpbindt <wesselbindt@gmail.com>
2021-01-27 22:06:28 +0100

    zig zag conversion solution

---
+++ zig_zag_conversion.py
@@ -0,0 +1,13 @@
+def convert(s: str, num_rows:int) -> str:
+    rows = [[] for _ in range(num_rows)]
+    for ix, c in enumerate(s):
+        ix_mod = ix % (2*num_rows - 2)
+        if 0 <= ix_mod < num_rows:
+            rows[ix_mod].append(c)
+        elif num_rows <= ix_mod:
+            rows[2*num_rows-2 - ix_mod].append(c)
+
+    return ''.join(''.join(row) for row in rows)
+
+assert convert('PAYPALISHIRING', 3) == 'PAHNAPLSIIGYIR'
+
