b2649045d970015dc0d1757f7a3fe93b0f2c4976
wpbindt <wesselbindt@gmail.com>
2021-01-29 15:18:51 +0100

    Notes on merge_k_lists

--- merge_k_lists.py
+++ merge_k_lists.py
@@ -1,6 +1,23 @@
 from __future__ import annotations
 from typing import Iterator, List, Optional
 
+# # # # Brute force
+# find minimum head val in k lists (O(k)) and put it in list
+# do this untill all nodes are gone
+# --> O(Nk) where N is the number of nodes :(
+
+# # # # Priority queue for all nodes
+# put all nodes in priority queue O(log(N))
+# remove minimal node O(log(N))
+# put minimal node in output O(1)
+# --> O(N log(N)) worse than brute force :(
+
+# # # # Priority queue for all lists
+# put all lists in priority queue ordered by value of head O(log(k))
+# take minimal list out, take its head off, and put it in the output list O(log(k))
+# put the decapitated list back in the priority queue O(log(k))
+# --> O(N log(k)) ^^
+
 
 class ListNode:
     def __init__(self, val: int = 0, next: Optional[ListNode] = None) -> None:
