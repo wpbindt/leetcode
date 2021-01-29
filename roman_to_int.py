e068e0f1d131cf30981ff6b59eb1e19cc6192e2f
wpbindt <wesselbindt@gmail.com>
2021-01-29 12:31:13 +0100

    Roman to integer solution

--- roman_to_int.py
+++ roman_to_int.py
@@ -1,7 +1,3 @@
-def roman_to_int(s: str) -> int:
-    ...
-
-
 roman_dict = {
     '': 0,
     'I': 1,
@@ -12,6 +8,19 @@ roman_dict = {
     'D': 500,
     'M': 1000,
 }
+
+def roman_to_int(s: str) -> int:
+    if len(s) <= 1:
+        return roman_dict[s]
+
+    first_digit = roman_dict[s[0]]
+    next_digit = roman_dict[s[1]]
+    if first_digit >= next_digit:
+        return first_digit + roman_to_int(s[1:])
+
+    return next_digit - first_digit + roman_to_int(s[2:])
+
+
 for roman_digit, arabic_numeral in roman_dict.items():
     for n in range(1, 4):
         assert roman_to_int(n * roman_digit) == n * arabic_numeral
