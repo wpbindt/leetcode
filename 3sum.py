b61d9afb89980513161f4bf5aa7d2adbdd8d4a66
wpbindt <wesselbindt@gmail.com>
2021-01-29 04:24:44 +0100

    Fix 3sum for realsies

--- 3sum.py
+++ 3sum.py
@@ -1,45 +1,11 @@
-from bisect import bisect_left
-from itertools import groupby
 from typing import List
-
-
-def bin_search(seq: List[int], item: int) -> int:
-    res = bisect_left(seq, item)
-    if res < len(seq):
-        if item == seq[res]:
-            return res
-
-    return -1
-
-
-def drop_duplicates(enum):
-    delete_stack = []
-    for ix, val in enumerate(enum[1:], 1):
-        if enum[ix - 1][1] == val[1]:
-            delete_stack.append(ix)
-
-    while delete_stack:
-        del enum[delete_stack.pop()]
 
 
 def three_sum(nums: List[int]) -> List[List[int]]:
     nums = sorted(nums)
-    output = []
+    output = set()
     
-    zero_ix = bin_search(nums, 0)
-    if zero_ix != -1:
-        try:
-            if nums[zero_ix + 1] == 0 and nums[zero_ix + 2] == 0:
-                output.append([0, 0, 0])
-        except IndexError:
-            ...
-
-    enum = list(enumerate(nums))
-    drop_duplicates(enum)
-    for ix, num in enum:
-        if num == 0:
-            continue
-
+    for ix, num in enumerate(nums):
         left = ix + 1
         right = len(nums) - 1
         while left < right:
@@ -48,12 +14,11 @@ def three_sum(nums: List[int]) -> List[List[int]]:
             elif nums[left] + num + nums[right] < 0 or left == ix:
                 left += 1
             else:
-                output.append([num, nums[left], nums[right]])
+                output.add((num, nums[left], nums[right]))
                 left += 1
                 right -= 1
 
-    return output
-
+    return list(map(list, output))
 
 assert three_sum([-4,-1,-1,0,1,2]) == [[-1,-1,2],[-1,0,1]]
 assert three_sum([0,0,0,0,0,0,0,0,0,0]) == [[0,0,0]]
