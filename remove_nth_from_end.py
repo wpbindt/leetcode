0af38651b26f6b3867e452ad468ba43e08486ec7
wpbindt <wesselbindt@gmail.com>
2021-01-29 11:38:02 +0100

    Add tests for remove nth node

--- remove_nth_from_end.py
+++ remove_nth_from_end.py
@@ -3,6 +3,11 @@ class ListNode:
         self.val = val
         self.next = next
 
+    def __iter__(self):
+        yield self.val
+        if self.next is not None:
+            yield from self.next
+
 
 def remove_from_end(head: ListNode, n: int) -> ListNode:
     tail = head
@@ -22,3 +27,24 @@ def remove_from_end(head: ListNode, n: int) -> ListNode:
 
     return head
 
+
+tail = ListNode(3, None)
+mid = ListNode(4, tail)
+head = ListNode(1, mid)
+head_removed = remove_from_end(head, 3)
+assert list(head_removed) == [4,3]
+
+tail = ListNode(3, None)
+mid = ListNode(4, tail)
+head = ListNode(1, mid)
+mid_removed = remove_from_end(head, 2)
+assert list(mid_removed) == [1,3]
+
+tail = ListNode(3, None)
+mid = ListNode(4, tail)
+head = ListNode(1, mid)
+tail_removed = remove_from_end(head, 1)
+assert list(tail_removed) == [1,4]
+
+head = ListNode(3, None)
+assert remove_from_end(head, 1) is None
