922e96ba9de381c2d082237ef48e520905b51ab4
wpbindt <wesselbindt@gmail.com>
2021-02-02 14:34:16 +0100

    Add parens generation solution notes

--- generate_parens.py
+++ generate_parens.py
@@ -1,5 +1,15 @@
 from typing import List
 
+# For n pairs: there are n different possibs:
+# first paren is paired with character in pos 1
+# first paren is paired with character 3
+# ...
+# first paren is paired with character 2n - 1
+# 
+# this leads to obv recursive solution, which has 
+# O(3^n) running time. Can be solved with dynamic
+# programming
+
 
 def generate_parens(n: int) -> List[str]:
     ...
