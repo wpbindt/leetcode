70db88affe5dcb118a7ec33f66e6c35f3fff94a5
wpbindt <wesselbindt@gmail.com>
2021-02-07 20:03:49 +0100

    Fix rotate matrix

--- rotate_matrix.py
+++ rotate_matrix.py
@@ -4,7 +4,7 @@ from typing import List
 def transpose(m: List[List[int]]) -> None:
     for i in range(len(m)):
         for j in range(i + 1, len(m)):
-            m[i][j] = m[j][i]
+            m[i][j], m[j][i] = m[j][i], m[i][j]
 
 def reverse_rows(m: List[List[int]]) -> None:
     for row in m:
