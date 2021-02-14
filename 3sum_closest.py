05d75c9a92f7126122f77c05ce3a33a733fa522a
wpbindt <wesselbindt@gmail.com>
2021-02-14 01:34:05 +0100

    3sum closest quadratic but better

--- 3sum_closest.py
+++ 3sum_closest.py
@@ -1,22 +1,19 @@
 def three_sum_closest(nums: List[int], target: int) -> int:
-    nums = sorted(nums)
-    for ix, num in enumerate(nums[1:-1], 1):
-        left_ix = ix - 1
-        right_ix = ix + 1
-        while left_ix != -1 and right_ix != len(nums):
-            if left_ix == -1 or right_ix == len(nums):
-                break
-
+    nums.sort()
+    closest = nums[0] + nums[1] + nums[2]
+    for ix, num in enumerate(nums[:-1]):
+        left_ix = ix + 1
+        right_ix = len(nums) - 1
+        while left_ix != right_ix:
             sum_ = nums[left_ix] + num + nums[right_ix]
-            if sum_ == target:
-                return target
-            if sum_ < target:
-                right_ix += 1
-                continue
+            if abs(closest - target) > abs(sum_ - target):
+                closest = sum_
             if sum_ > target:
+                right_ix -= 1
+            elif sum_ > target:
                 left_ix -= 1
-
-        closest = min({sum_, closest}, key=lambda x: abs(target - x))
+            else:
+                return target
     
     return closest
 
